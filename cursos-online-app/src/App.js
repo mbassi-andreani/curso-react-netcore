2a0e07851ba366416065b325bffc3862ea54a58b
Martin Bassi <mbassi@andreani.com>
2020-11-09 16:55:00 -0300

    Demo Api Paises del mundo - Axios

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <h1>Bienvenido {nombre} al curso de ASP.NET Core y React Hooks! </h1>
       <input type="text" name="nombre" onChange={eventoCajaTexto} value={nombre} />
       {/* Pasando propiedades de padres a hijos */}
-      <Perfil atributo={nombre}/>
+      <Perfil/>
     </div>
   );
 }
--- cursos-online-app/src/componentes/Perfil.js
+++ cursos-online-app/src/componentes/Perfil.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 /* useEffect trabaja cada vez que se produce un cambio dentro de las propiedades del componente */
 function Perfil(props) {
+    const [paises, obtenerPaises] = useState([]);
+    const [status, cambiarStatus] = useState(false); //cuando cargues la página, no va a estar cargado
+
     useEffect(()=> {
-        document.title = props.atributo //cada vez que la propiedad atributo cambie, actualiza el titulo de la página
-    }, [props.atributo])
-    return (
-        <div style={{background:"yellow"}}>
-        Este es el perfil de {props.atributo}
-        </div>
-    );
+        axios.get('https://restcountries.eu/rest/v2/all').then(resultado => {
+            obtenerPaises(resultado.data);
+            cambiarStatus(true);
+        }).catch(error => {
+            cambiarStatus(true);
+        })
+        
+    }, []) //si en el segundo parámetro dejás los corchetes vacíos, el useEffect se ejecuta una sola vez porque no le pasas una propiedad para que la evalue
+    if(status) {
+        return (
+            <ul>
+               {paises.map((pais,indice) => 
+               <li key={indice}>{pais.name}</li>
+               )} 
+            </ul>
+        );
+    } else {
+        return (<h1>Obteniendo paises...</h1>);
+    }
 }
 
 export default Perfil;
